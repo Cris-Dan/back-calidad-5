a969372d5e63ffa4e6181cfc899bffb9648446c4
Cris-Dan <cristian.cornejo@unmsm.edu.pe>
2019-06-30 22:51:46 +0000

    curso se busca por id

--- routes/curso.js
+++ routes/curso.js
@@ -7,7 +7,7 @@ const cursoController = require('../controllers/curso.controller');
 
 router.get('/curso/obtenerCursos',cursoController.buscarTodo);
 router.post('/curso',cursoController.crearCurso);
-router.get('/curso/:nombre',cursoController.buscarCurso);
+router.get('/curso/:id',cursoController.buscarCurso);
 router.put('/curso/actualizar/:nombre',cursoController.actualizarCurso);
 router.delete('/curso/eliminar/:nombre',cursoController.eliminarCurso);
 
